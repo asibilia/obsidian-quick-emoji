0ed8c60527b75cb311c720cf5e46ab24bb216e72
Alec Sibilia <sibilia.alec@gmail.com>
2025-04-15 11:11:49 -0400

    fix(root): eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,57 +1,66 @@
-import js from "@eslint/js";
-// import importPlugin from "eslint-plugin-import";
-// import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import typescript from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+import typescript from 'typescript-eslint'
 
 export default defineConfig([
 	{
-		files: ["**/*.{js,mjs,cjs,ts}"],
-		plugins: { js },
-		extends: ["js/recommended"],
-		// rules: {
-		// 	// Import
-		// 	"import/order": [
-		// 		"error",
-		// 		{
-		// 			groups: ["builtin", "external", "internal", "sibling"],
-		// 			pathGroups: [
-		// 				{
-		// 					pattern: "obsidian",
-		// 					group: "external",
-		// 					position: "before",
-		// 				},
-		// 				{
-		// 					pattern: "~/**",
-		// 					group: "internal",
-		// 				},
-		// 			],
-		// 			pathGroupsExcludedImportTypes: ["obsidian"],
-		// 			"newlines-between": "always",
-		// 			alphabetize: {
-		// 				order: "asc",
-		// 				caseInsensitive: true,
-		// 			},
-		// 		},
-		// 	],
-		// 	// Prettier
-		// 	"prettier/prettier": [
-		// 		"error",
-		// 		{
-		// 			semi: false,
-		// 			singleQuote: true,
-		// 			trailingComma: "es5",
-		// 			endOfLine: "lf",
-		// 			printWidth: 80,
-		// 			tabWidth: 4,
-		// 		},
-		// 	],
-		// },
+		files: ['**/*.{js,mjs,cjs,ts}'],
+		plugins: { js, prettier, import: importPlugin },
+		extends: ['js/recommended'],
 	},
 	{
-		files: ["**/*.{js,mjs,cjs,ts}"],
+		files: ['**/*.{js,mjs,cjs,ts}'],
 		languageOptions: { globals: globals.browser },
 	},
 	...typescript.configs.recommended,
-]);
+	// RULES
+	{
+		files: ['**/*.{js,mjs,cjs,ts}'],
+		rules: {
+			// Typescript
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+			],
+			// Prettier
+			'prettier/prettier': [
+				'error',
+				{
+					semi: false,
+					singleQuote: true,
+					trailingComma: 'es5',
+					endOfLine: 'lf',
+					printWidth: 80,
+					tabWidth: 4,
+				},
+			],
+			// Import
+			'import/order': [
+				'error',
+				{
+					groups: ['builtin', 'external', 'internal', 'sibling'],
+					pathGroups: [
+						{
+							pattern: 'obsidian',
+							group: 'external',
+							position: 'before',
+						},
+						{
+							pattern: '~/**',
+							group: 'internal',
+						},
+					],
+					pathGroupsExcludedImportTypes: ['obsidian'],
+					'newlines-between': 'always',
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+				},
+			],
+		},
+	},
+])
